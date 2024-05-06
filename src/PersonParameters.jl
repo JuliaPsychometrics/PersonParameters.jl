288f3a43bd00dd9ab7211509e7d77b2adc2212fc
p-gw <p.gewessler@gmx.at>
2024-05-06 11:09:12 +0200

    implement interface for single person parameter

--- src/PersonParameters.jl
+++ src/PersonParameters.jl
@@ -4,45 +4,67 @@ using Roots: find_zero
 using ItemResponseFunctions:
     ItemResponseModel, OneParameterLogisticModel, irf, iif, information
 
-export MLE, WLE, person_parameters
+export MLE, WLE, person_parameters, person_parameter
 export OneParameterLogisticModel
-
-
-
+export value, se, score
 
 abstract type PersonParameterAlgorithm end
 
 function optfun end
 function se end
 
-struct PersonParameterResult{T<:ItemResponseModel,U<:Real,V<:PersonParameterAlgorithm}
+struct PersonParameter{U<:Real}
+    value::U
+    se::U
+    score::Int
+end
+
+value(pp::PersonParameter) = pp.value
+se(pp::PersonParameter) = pp.se
+score(pp::PersonParameter) = pp.score
+
+struct PersonParameterResult{
+    T<:ItemResponseModel,
+    U<:PersonParameter,
+    V<:PersonParameterAlgorithm,
+}
     values::Vector{U}
-    standard_errors::Vector{U}
     algorithm::V
 end
 
+Base.getindex(pp::PersonParameterResult, i) = getindex(pp.values, i)
+
 function person_parameters(
     T::Type{OneParameterLogisticModel},
+    scores::AbstractVector{Int},
+    betas::AbstractVector{U},
+    alg::PersonParameterAlgorithm,
+) where {U<:Real}
+    thetas = [person_parameter(T, score, betas, alg) for score in scores]
+    return PersonParameterResult{T,eltype(thetas),typeof(alg)}(thetas, alg)
+end
+
+function person_parameter(
+    T::Type{OneParameterLogisticModel},
+    score::Int,
     betas::AbstractVector{U},
     alg::PersonParameterAlgorithm,
 ) where {U<:Real}
     I = length(betas)
-    thetas = zeros(U, I + 1)
-    standard_errors = similar(thetas)
     init_x = zero(U)
 
-    for r in eachindex(thetas)
-        if (r == 1 || r == I + 1) && !rational_bounds(alg)
-            thetas[r] += NaN
-            standard_errors[r] += NaN
-            continue
+    if !rational_bounds(alg)
+        if score == 0
+            return PersonParameter(-Inf, Inf, score)
+        elseif score == I
+            return PersonParameter(Inf, Inf, score)
         end
-
-        thetas[r] += find_zero(x -> optfun(alg, T, x, betas, r), init_x)
-        standard_errors[r] += se(alg, T, thetas[r], betas)
     end
 
-    return PersonParameterResult{T,U,typeof(alg)}(thetas, standard_errors, alg)
+    theta = find_zero(x -> optfun(alg, T, x, betas, score + 1), init_x)
+    standard_error = se(alg, T, theta, betas)
+
+    return PersonParameter(theta, standard_error, score)
 end
 
 include("mle.jl")
