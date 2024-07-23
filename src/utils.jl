0bb671fd738927f012510007747f0957064ffa92
Philipp Gewessler <p-gw@users.noreply.github.com>
2024-07-23 10:31:23 +0200

    use likelihood functions from ItemResponseFunctions (#24)

--- src/utils.jl
+++ src/utils.jl
@@ -3,24 +3,3 @@ function get_unique_response_patterns(responses::AbstractVector)
     id_map = [findall(x -> isequal(x, pattern), responses) for pattern in unique_patterns]
     return unique_patterns, id_map
 end
-
-function likelihood(M::Type{<:ItemResponseModel}, theta::Real, betas, responses)
-    L = one(theta)
-    for (beta, y) in zip(betas, responses)
-        L *= irf(M, theta, beta, y)
-    end
-    return L
-end
-
-function loglik(M::Type{<:ItemResponseModel}, theta::Real, betas, responses)
-    L = zero(theta)
-    for (beta, y) in zip(betas, responses)
-        L += log(irf(M, theta, beta, y))
-    end
-    return L
-end
-
-function derivative_loglik(M::Type{<:ItemResponseModel}, theta::Real, betas, responses)
-    f(x) = loglik(M, x, betas, responses)
-    return derivative(f, theta)
-end
